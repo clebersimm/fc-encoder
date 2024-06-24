fd53f3fad15ed4ee66441f75604534fd2cebd379
Cleber Simm <clebersimm@gmail.com>
2024-06-24 13:28:19 -0300

    Update video.go

--- domain/video.go
+++ domain/video.go
@@ -11,7 +11,7 @@ type Video struct {
 	ResourceID string    `json:"resource_id" valid:"notnull" gorm:"type:varchar(255)"`
 	FilePath   string    `json:"file_path" valid:"notnull" gorm:"type:varchar(255)"`
 	CreatedAt  time.Time `json:"-" valid:"-"`
-	Jobs []*Job `json:"-" valid:"-"`
+	Jobs []*Job `json:"-" valid:"-" gorm:"ForeignKey:VideoID"`
 }
 
 func init() {
